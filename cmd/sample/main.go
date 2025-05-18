5f7f045e5bdd8195be5cbc1fd20295f713e84df6
Occccn <big.island.yz@gmail.com>
2025-05-18 11:27:40 +0900

    [update] sample/mainでpkgのimport検証するよう修正

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/Occccn/zero_deeplearning2/pkg/pjt2"
 	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
-	a := mat.NewDense(2, 2, []float64{1, 2, 3, 4})
-	b := mat.NewDense(2, 2, []float64{5, 6, 7, 8})
-
-	var c mat.Dense
-	c.Mul(a, b)
-
-	fmt.Printf("Result:\n%v\n", mat.Formatted(&c))
+	a := mat.NewVecDense(4, []float64{1, 2, 3, 4})
+	b := mat.NewVecDense(4, []float64{2, 4, 6, 8})
+	cosineSim := pjt2.CosineSimilarity(a, b)
+	fmt.Printf("Cosine Similarity: %v\n", cosineSim)
 }
