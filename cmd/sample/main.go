1d3438e156dfb77b2ae66e01430bcaf8ac9f0e62
Occccn <big.island.yz@gmail.com>
2025-05-25 10:57:56 +0900

    init

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/Occccn/zero_deeplearning2/pkg/pjt2"
+	"github.com/Occccn/zero_deeplearning2/pkg/similarity"
 	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
 	a := mat.NewVecDense(4, []float64{1, 2, 3, 4})
 	b := mat.NewVecDense(4, []float64{2, 4, 6, 8})
-	cosineSim := pjt2.CosineSimilarity(a, b)
+	cosineSim := similarity.CosineSimilarity(a, b)
 	fmt.Printf("Cosine Similarity: %v\n", cosineSim)
 }
