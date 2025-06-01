9d5fca8f39ad341de0a89ced2089c05dba5727b9
Occccn <big.island.yz@gmail.com>
2025-06-01 21:07:23 +0900

    matmal追加

---
+++ pkg/layer/matmal.go
@@ -0,0 +1,33 @@
+package layer
+
+import (
+	"gonum.org/v1/gonum/mat"
+)
+
+type Matmal struct {
+	Params *mat.Dense
+	Grad   *mat.Dense
+	X      *mat.Dense
+}
+
+func (matmal *Matmal) Forward(x *mat.Dense) *mat.Dense {
+	// np.dot(x, matmal.Params)
+	output := mat.NewDense(x.RawMatrix().Rows, matmal.Params.RawMatrix().Cols, nil)
+	output.Mul(x, matmal.Params)
+	// matmal.X = x
+	matmal.X = mat.NewDense(x.RawMatrix().Rows, x.RawMatrix().Cols, nil)
+	matmal.X.Copy(x)
+	return output
+}
+
+func (matmal *Matmal) Backward(grad *mat.Dense) *mat.Dense {
+	// np.dot(grad, matmal.Params.T)
+	dx := mat.NewDense(grad.RawMatrix().Rows, matmal.X.RawMatrix().Cols, nil)
+	dx.Mul(grad, matmal.Params.T())
+
+	// matmal.Grad = np.dot(matmal.X.T, grad)
+	matmal.Grad = mat.NewDense(matmal.X.RawMatrix().Cols, grad.RawMatrix().Cols, nil)
+	matmal.Grad.Mul(matmal.X.T(), grad)
+
+	return dx
+}
