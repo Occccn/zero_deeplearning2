27d5361a3285cdb02fd992bc8ebec3ab62f55f3f
Occccn <big.island.yz@gmail.com>
2026-01-12 15:22:51 +0900

    init

---
+++ pkg/utils/preprocess.go
@@ -0,0 +1,5 @@
+package utils
+
+func Preprocess(text string) string {
+	return text
+}
---
+++ pkg/utils/preprocess_test.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestPreprocess(t *testing.T) {
+	tests := []struct {
+		name string
+		text string
+		want string
+	}{
+		{
+			name: "Test Preprocess",
+			text: "Hello, world",
+			want: "Hello, world",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Preprocess(tt.text)
+			if got != tt.want {
+				t.Errorf("Preprocess() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
