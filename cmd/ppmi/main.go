80a96443b0e69ccf780857ee2a4e3a916c6862c6
Occccn <big.island.yz@gmail.com>
2026-01-18 14:29:28 +0900

    add svd process

--- cmd/ppmi/main.go
+++ cmd/ppmi/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Occccn/zero_deeplearning2/pkg/utils"
+	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
@@ -11,6 +12,19 @@ func main() {
 	corpus, _, _ := utils.Preprocess(text)
 	co_matrix := utils.CreateCoMatrix(corpus, 7, 1)
 	ppmi_matrix := utils.PPMICalculation(co_matrix)
+
+	// SVD分解
+	var svd mat.SVD
+	ok := svd.Factorize(ppmi_matrix, mat.SVDFull)
+	if !ok {
+		panic("SVD failed")
+	}
+
+	// 結果を取得
+	u := mat.Dense{}
+	svd.UTo(&u) // 左特異ベクトル U
+	fmt.Println("U:")
+	fmt.Println(u)
 	fmt.Println("co_matrix:")
 	fmt.Println(co_matrix)
 	fmt.Println("ppmi_matrix:")
