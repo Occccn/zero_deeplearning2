61eee47dde73df7026152d322fcd8f99794046a7
Occccn <big.island.yz@gmail.com>
2026-01-18 15:33:43 +0900

    support for window_size=2

--- pkg/utils/create_contexts_target.go
+++ pkg/utils/create_contexts_target.go
@@ -5,18 +5,19 @@ import (
 )
 
 func CreateContextsTarget(corpus []int64, window_size int) *mat.Dense {
-	if len(corpus)-2 < 0 {
+	rowCount := len(corpus) - 2*window_size
+	if rowCount < 1 {
 		panic("corpus is too short")
 	}
-	contexts := mat.NewDense(len(corpus)-2, window_size*2, nil)
-	for i := range len(corpus) - 2 {
-		colIdx := 0 // 列インデックスを別でカウント
+	contexts := mat.NewDense(rowCount, window_size*2, nil)
+	for i := range rowCount {
+		colIdx := 0
+		targetIdx := i + window_size // target の位置
 		for j := -window_size; j <= window_size; j++ {
 			if j == 0 {
 				continue
 			}
-			idx := i + j + 1
-			contexts.Set(i, colIdx, float64(corpus[idx]))
+			contexts.Set(i, colIdx, float64(corpus[targetIdx+j]))
 			colIdx++
 		}
 	}
