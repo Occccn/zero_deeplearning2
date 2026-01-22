0706713eff2d6dd0c327152284c6d6f960042534
Occccn <big.island.yz@gmail.com>
2026-01-22 10:21:21 +0900

    add targets to response

--- pkg/utils/create_contexts_target.go
+++ pkg/utils/create_contexts_target.go
@@ -4,13 +4,15 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-func CreateContextsTarget(corpus []int64, window_size int) *mat.Dense {
+func CreateContextsTarget(corpus []int64, window_size int) (*mat.Dense, *mat.Dense) {
 	rowCount := len(corpus) - 2*window_size
 	if rowCount < 1 {
 		panic("corpus is too short")
 	}
 	contexts := mat.NewDense(rowCount, window_size*2, nil)
+	target := mat.NewDense(rowCount, 1, nil)
 	for i := range rowCount {
+		target.Set(i, 0, float64(corpus[i+window_size]))
 		colIdx := 0
 		targetIdx := i + window_size // target の位置
 		for j := -window_size; j <= window_size; j++ {
@@ -21,5 +23,5 @@ func CreateContextsTarget(corpus []int64, window_size int) *mat.Dense {
 			colIdx++
 		}
 	}
-	return contexts
+	return contexts, target
 }
