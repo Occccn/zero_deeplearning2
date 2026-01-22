b8c5bd53df967d40d91705e6b5da67436c4fe81d
Occccn <big.island.yz@gmail.com>
2026-01-22 10:39:03 +0900

    update

--- cmd/convert_one_hot/main.go
+++ cmd/convert_one_hot/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Occccn/zero_deeplearning2/pkg/utils"
 )
 
 func main() {
 	text := "You say goodbye and I say hello."
-	corpus, _, _ := utils.Preprocess(text)
+	corpus, word_to_id, _ := utils.Preprocess(text)
 	contexts, target := utils.CreateContextsTarget(corpus, 1)
+	fmt.Println(target)
+	target_one_hot := utils.ConvertOneHot(target, len(word_to_id))
+	contexts_one_hot := utils.ConvertOneHot(contexts, len(word_to_id))
+	fmt.Println(target_one_hot)
+	fmt.Println(contexts_one_hot)
 }
